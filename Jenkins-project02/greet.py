819f6eec587185412f4908076e3491f86df942c8
Ubuntu <ubuntu@ip-172-31-16-10.ec2.internal>
2026-05-07 13:06:33 +0000

    second

--- Jenkins-project02/greet.py
+++ Jenkins-project02/greet.py
@@ -5,6 +5,7 @@ import datetime
 def greet():
     now = datetime.datetime.now()
     print(f"Hello from Jenkins!")
+    print(f"This is second commit!")
     print(f"Script ran at: {now}")
     print("Python is working inside Jenkins!")
 
