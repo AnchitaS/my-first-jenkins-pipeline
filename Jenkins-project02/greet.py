141535a18cabb1b49641f6cf0b93102b6d3632cf
Ubuntu <ubuntu@ip-172-31-16-10.ec2.internal>
2026-05-07 11:44:11 +0000

    first

---
+++ Jenkins-project02/greet.py
@@ -0,0 +1,12 @@
+# greet.py — Jenkins will run this file
+
+import datetime
+
+def greet():
+    now = datetime.datetime.now()
+    print(f"Hello from Jenkins!")
+    print(f"Script ran at: {now}")
+    print("Python is working inside Jenkins!")
+
+if __name__ == "__main__":
+    greet()
